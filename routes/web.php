5d14391703d5adba9681d6fdb7f13be8691a7ce9
Alok Kumar <alok2022ncrb@outlook.com>
2024-02-12 20:13:50 +0530

    added user auth

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-Route::view('/', 'home');
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
